a1ccd246e3da5b5b634af891caca1856c7e36fb7
michael kan <michael.kan.mail@gmail.com>
2023-05-15 18:05:53 +0200

    writer.c

---
+++ finder-app/writer.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <syslog.h>
+
+void writetofile(const char* filepath, const char* str) {
+    openlog("finder-app", LOG_PID, LOG_USER);
+    int fd = open(filepath, O_CREAT | O_RDWR, 0666);
+    if (fd == -1) {
+        syslog(LOG_ERR, "unable to create file, error code: %d", fd);
+        return;
+    }
+    char tmp;
+    size_t count = 0;
+    do {
+        tmp = str[count];
+        count++;
+    } while (tmp != '\0');
+    
+    write(fd, str, count);
+    syslog(LOG_DEBUG, "Writing \"%s\" to \"%s\" file", str, filepath);
+    closelog();
+    int err = close(fd);
+    if (err != -1) {
+        syslog(LOG_ERR, "unable to close file, error code: %d", err);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("you have to define first and second arguments\n");
+        return 1;
+    }
+    writetofile(argv[1], argv[2]);
+}
