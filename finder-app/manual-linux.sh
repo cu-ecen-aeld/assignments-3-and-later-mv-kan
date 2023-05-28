2163559e4ecf71edc5dd2922e5fac2dc8d1230fa
michael kan <michael.kan.mail@gmail.com>
2023-05-28 09:32:01 +0200

    a

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -10,7 +10,7 @@ KERNEL_VERSION=v5.15
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
 then
@@ -95,7 +95,8 @@ sudo mknod -m 600 dev/console c 5 1
 ln -sf dev/null dev/tty2
 
 # Add dependencies
-LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox |grep "NEEDED"| grep -o -P '(?<=\[)[^\]]+(?=\])')
+LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox |grep "Shared library"| grep -o -P '(?<=\[)[^\]]+(?=\])')
+LIBS="${LIBS}"$'\n'$(${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter" | awk -F': ' '{gsub(/\]/, "", $2); print $2}')
 echo "Needed paths $LIBS"
 # Iterate over the library names
 while IFS= read -r library; do
@@ -106,11 +107,11 @@ while IFS= read -r library; do
   echo "available paths ${library_path}"
   target_line=$(echo "$library_path" | grep -m 1 "${CROSS_COMPILE%?}")
   # Copy the library to the desired location
-  output_lib="${OUTDIR}/rootfs/lib/${library}"
+  output_lib="${OUTDIR}/rootfs/lib/$(basename "$library")"
   if [ ! -f "${OUTDIR}/rootfs/lib/${library}" ] 
   then
-    cp "$target_line" "${OUTDIR}/rootfs/lib/${library}"
-    echo  "copy $target_line into $output_lib"
+    cp "$target_line" "$output_lib"
+    echo "copy $target_line into $output_lib"
   fi 
 done <<< "$LIBS"
 # TODO: Clean and build the writer utility
